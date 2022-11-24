94027d2b44f3f5fd3fb453f36f283b2460d66622
Shadi Samadi <68650974+s-samadi@users.noreply.github.com>
2022-11-24 16:39:21 +1100

    Update zipslip.cs

--- tests/csharp/zipslip.cs
+++ tests/csharp/zipslip.cs
@@ -23,7 +23,7 @@ class Program
 			foreach (ZipArchiveEntry entry in zipArchive.Entries)
 			{
 			
-				string extractPath = Path.Combine(destination, entry.FullName); 
+				string extractPath = Path.Combine(destination, entry.FullName); //codeql 
 				entry.ExtractToFile(extractPath);
 			}
 		}
