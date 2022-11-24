46a28860edb2e66bcb52aa8aaa327728cc8de841
Shadi Samadi <68650974+s-samadi@users.noreply.github.com>
2022-11-24 16:40:19 +1100

    Update server.js

--- tests/javascript/server.js
+++ tests/javascript/server.js
@@ -4,7 +4,7 @@ const { Pool } = require('pg')
 
 const pool = new Pool({
     
-    user: 'dbuser', 
+    user: 'dbuser', //codeql
     host: 'database.server.com',
     database: 'mydb',
     password: process.env.POSTGRES_PASSWORD,
